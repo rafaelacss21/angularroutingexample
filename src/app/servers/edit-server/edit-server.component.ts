79de71ad652108a584b3f7ca6dfaecd6968cc9fd
RafaelaSilveira <rafaelacss21@gmail.com>
2019-04-04 15:57:53 -0400

    outsourcing the route configuration

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService, private route: ActivatedRoute) {
 
